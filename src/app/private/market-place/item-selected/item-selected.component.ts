b5a7c60536285febdc85c1aed01e562b2b4954f0
Brajesh Maharana <maharana.brajesh@gmail.com>
2022-01-17 19:38:44 +0530

    ndvdgf

--- src/app/private/market-place/item-selected/item-selected.component.ts
+++ src/app/private/market-place/item-selected/item-selected.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/services/market_services/market_modal';
+import * as fromActions from '../../../store/product_store/actions';
+import * as fromStore from '../../../store/product_store/reducer';
+import * as fromSelector from '../../../store/product_store/selecter';
 @Component({
   selector: 'app-item-selected',
   templateUrl: './item-selected.component.html',
@@ -8,11 +14,38 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class ItemSelectedComponent implements OnInit {
   item: string[] = ['Grey', 'Red', 'Yellow'];
+  // id: any
+  isLoading$: Observable<boolean>;
+  error$: Observable<string | null>;
+  productsid$: Observable<Product>;
+  
 
-  constructor() { }
+  // productsid$: string
+  constructor(
+    private route: ActivatedRoute,
+    private store: Store<fromStore.ProductState>
+  ) { }
 
   ngOnInit(): void {
+    this.getProduct();
   }
+
+  getProduct(){
+    // this.products$ = this.store.select(fromSelector.products);
+    // this.isLoading$ = this.store.select(fromSelector.isLoading);
+    // this.store.dispatch(fromActions.Load_Product_detailes({ id: "1" }));
+    // this.productsid$ = this.store.select(fromSelector.getdetails$);
+    // this.store.select(fromSelector.getdetails$).subscribe(
+    //   (id: any) => {
+    //       this.productsid$=id;
+    //   }
+    //  );
+    this.productsid$ = this.store.select(fromSelector.getdetails$);
+
+    // this.productsid$ = this.store.select(fromSelector.getdetails$);
+    // console.log(this.productsid$)
+  }
+
   description(){
     document.getElementById("description")?.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
